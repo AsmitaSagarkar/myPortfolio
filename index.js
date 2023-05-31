6ae34fbfd1baec7e78f579273fa189b221f2818d
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-05-31 23:11:48 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//toggle navbar
+
+const mobile_nav = document.querySelector(".mobilenav");
+const header = document.querySelector(".header");
+toggleNavbar=()=>{
+    header.classList.toggle("active");
+}
+mobile_nav.addEventListener("click",()=>
+toggleNavbar()
+);
+
+//text animation
+let title= document.querySelector('.para');
+let content = "I'm Asmita Sagarkar. I'm a passionate fullstack web-developer from Indore.";
+let index=1;
+// var audio = new audio("Keyboard-Typing.mp3");
+const typeWriter=()=>{
+    let new_title = content.slice(0,index);
+    title.innerText = new_title;
+    //index++;
+    index>content.length?index=1:index++;
+
+    setTimeout(() =>typeWriter(), 100);
+    // setTimeout(()=>audio.play,100);
+}
+typeWriter();
