b21d1e5a6a382e6d953074d8dffc5be0ff8c0559
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-06-09 00:41:01 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -10,11 +10,23 @@ toggleNavbar()
 );
 
 //text animation
+const mediaQuery = window.matchMedia('(max-width:60rem)');
 let title= document.querySelector('.para');
 let content = "I'm Asmita Sagarkar. I'm a passionate fullstack web-developer from Indore.";
 let index=1;
 // var audio = new audio("Keyboard-Typing.mp3");
 const typeWriter=()=>{
+
+    if(mediaQuery.matches){
+    let new_title = content.slice(0,index);
+    title.innerText = new_title;
+    index++;
+    // index>content.length?index=1:index++;
+    
+
+    setTimeout(() =>typeWriter(), 100);
+    }
+    else{
     let new_title = content.slice(0,index);
     title.innerText = new_title;
     //index++;
@@ -22,5 +34,6 @@ const typeWriter=()=>{
 
     setTimeout(() =>typeWriter(), 100);
     // setTimeout(()=>audio.play,100);
+    }
 }
 typeWriter();
