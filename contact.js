b84eacd08d9edcf0f0318a9e53e1c8f018707721
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-08-04 18:40:18 +0530

    Resume and linkedin link added

--- contact.js
+++ contact.js
@@ -10,17 +10,6 @@ github.addEventListener("click",()=>{
     }, 200);
 })
 
-//linkedin
-const linkedin = document.querySelector("#linkedin");
-linkedin.addEventListener("click",()=>{
-    const url = "https://www.linkedin.com/in/asmita-sagarkar-949a08201";
-    linkedin.style.color = "#27ae60";
-    setTimeout(() => {
-        linkedin.style.color = "aliceblue";
-        window.open(url,"_blank");
-        
-    }, 200);
-})
 
 //twitter
 const twitter = document.querySelector("#twitter");
@@ -41,6 +30,29 @@ mail.addEventListener("click",()=>{
     mail.style.color = "#27ae60";
     setTimeout(() => {
         mail.style.color = "aliceblue";
+        window.open(url,"_blank");
+        
+    }, 200);
+})
+//linkedin
+const linkedin = document.querySelector("#linkedin");
+linkedin.addEventListener("click",()=>{
+    const url = "https://www.linkedin.com/in/asmita-sagarkar-949a08201";
+    linkedin.style.color = "#27ae60";
+    setTimeout(() => {
+        linkedin.style.color = "aliceblue";
+        window.open(url,"_blank");
+        
+    }, 200);
+})
+
+//resume
+const resume = document.querySelector("#resume");
+resume.addEventListener("click",()=>{
+    const url = "https://drive.google.com/file/d/1p5ZVhQAr6gnPuujCTYXJ42kexJl1e1t6/view";
+    resume.style.color = "#27ae60";
+    setTimeout(() => {
+        resume.style.color = "aliceblue";
         window.open(url,"_blank");
         
     }, 200);
